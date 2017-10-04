27cb7bc43bd247897ce2966dab82f3d1fd8734eb
Fernando Beck <fe.beck25@gmail.com>
2017-10-04 10:33:15 -0300

    Make custom validator for size

--- product-catalog/server/models/product.js
+++ product-catalog/server/models/product.js
@@ -2,10 +2,18 @@
 
 module.exports = function (Product) {
   // ^(https?://)?(?:[a-z0-9\\-]+\\.)+[a-z]{2,6}(?:/[^/#?]+)+\\.(?:jpe?g|gif|png)$
-  //              |--------- domain ------------|---- path ----|--- extension ---|
+  // |- protocol -|--------- domain ------------|---- path ----|--- extension ---|
   let imageUrlValidator = '^(https?://)?(?:[a-z0-9\\-]+\\.)+[a-z]{2,6}(?:/[^/#?]+)+\\.(?:jpe?g|gif|png)$'
   Product.validatesFormatOf('productImageUrl', {
     with: imageUrlValidator,
     message: 'Must be a valid URL with valid image file extension (jpg, jpeg, png, gif)'
   })
+
+  function sizeValidator (err) {
+    let isString = (variable) => typeof (variable) === 'string'
+    let isArray = variable => Array.isArray(variable)
+    if (!isString(this.size) && !isArray(this.size)) err()
+  }
+
+  Product.validate('size', sizeValidator, {message: 'Size must be either a string or an array'})
 }
