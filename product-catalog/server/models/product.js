ac2303ece9f0e384cae15aec282e34318a480a11
Fernando Beck <fe.beck25@gmail.com>
2017-10-04 09:21:20 -0300

    Validate productImageUrl field

--- product-catalog/server/models/product.js
+++ product-catalog/server/models/product.js
@@ -1,5 +1,11 @@
-'use strict';
+'use strict'
 
-module.exports = function(Product) {
-
-};
+module.exports = function (Product) {
+  // ^(https?://)?(?:[a-z0-9\\-]+\\.)+[a-z]{2,6}(?:/[^/#?]+)+\\.(?:jpe?g|gif|png)$
+  //              |--------- domain ------------|---- path ----|--- extension ---|
+  let imageUrlValidator = '^(https?://)?(?:[a-z0-9\\-]+\\.)+[a-z]{2,6}(?:/[^/#?]+)+\\.(?:jpe?g|gif|png)$'
+  Product.validatesFormatOf('productImageUrl', {
+    with: imageUrlValidator,
+    message: 'Must be a valid URL with valid image file extension (jpg, jpeg, png, gif)'
+  })
+}
